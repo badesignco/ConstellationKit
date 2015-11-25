ed9c21c5845f3c1914d2b0e9d95b6616405172c9
badesignco <ben@constellation.software>
2015-11-25 07:25:33 -0500

    Initial Commit

---
+++ ConstellationKit/ConstellationKit.h
@@ -0,0 +1,19 @@
+//
+//  ConstellationKit.h
+//  ConstellationKit
+//
+//  Created by Ben Alexander on 11/25/15.
+//  Copyright © 2015 Constellation Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConstellationKit.
+FOUNDATION_EXPORT double ConstellationKitVersionNumber;
+
+//! Project version string for ConstellationKit.
+FOUNDATION_EXPORT const unsigned char ConstellationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConstellationKit/PublicHeader.h>
+
+
